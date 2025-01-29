5d8dc5e118820e652ad0f744735e6b7e66dfcb70
prudvicivil <139361766+prudvicivil@users.noreply.github.com>
2025-01-29 11:43:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIARYEUCDKYI4XQ56SB"
-  secret_key = "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"
+  access_key = "*******"
+  secret_key = "*******"
 }
